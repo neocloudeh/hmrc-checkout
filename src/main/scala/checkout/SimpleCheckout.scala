9e992d7ec27cbef5b7f7d808431b3b5175e5fa92
Elias Court <elias@propertypartner.co>
2016-08-05 15:07:26 +0100

    step 2

--- src/main/scala/checkout/SimpleCheckout.scala
+++ src/main/scala/checkout/SimpleCheckout.scala
@@ -7,8 +7,19 @@ package checkout
 object SimpleCheckout extends CheckoutSystem {
   val products = Map(("apple", BigDecimal(0.25)), ("orange", BigDecimal(2.05)))
 
-  override def checkout(purchases: Seq[String]): BigDecimal = purchases.foldLeft(BigDecimal(0)){
-    case (subtotal, product) =>
-      products.getOrElse(product, throw new IllegalArgumentException(s"$products is not a supported item.")) + subtotal
+  override def checkout(purchases: Seq[String]): BigDecimal = {
+    val zeroState: (Map[String, Long], BigDecimal) = (Map().withDefaultValue(0L), BigDecimal(0))
+
+    val (finalItems, finalSubtotal) = purchases.foldLeft(zeroState) {
+      case ((itemsSoFar, totalSoFar), product) =>
+        val newItemsSoFar = itemsSoFar.updated(product, itemsSoFar(product) + 1)
+        val newSubtotal = products.getOrElse(product, throw new IllegalArgumentException(s"$products is not a supported item.")) + totalSoFar
+        (newItemsSoFar, newSubtotal)
+    }
+
+    val orangesDiscount = Math.floor(finalItems("orange") / 3) * products("orange")
+    val appleDiscount = Math.floor(finalItems("apple") / 2) * products("apple")
+
+    finalSubtotal - orangesDiscount - appleDiscount
   }
 }
--- src/test/scala/checkout/CheckoutSpec.scala
+++ src/test/scala/checkout/CheckoutSpec.scala
@@ -18,7 +18,7 @@ class CheckoutSpec extends FlatSpec with Matchers with PropertyChecks {
     }
   }
 
-  "A checkout" should "return the correct total in the correct case" in {
+  "A checkout" should "return the correct total in the standard correct case" in {
     assert(SimpleCheckout.checkout(List("apple", "orange")) === BigDecimal(2.30))
   }
 
@@ -26,5 +26,29 @@ class CheckoutSpec extends FlatSpec with Matchers with PropertyChecks {
     assertThrows[IllegalArgumentException] { // Result type: Assertion
       SimpleCheckout.checkout(List("apple", "banana", "orange"))
     }
+  }
+
+  "A checkout" should "correctly apply apple discounts if two are purchased" in {
+    assert(SimpleCheckout.checkout(List("apple", "apple")) === BigDecimal(0.25))
+  }
+
+  "A checkout" should "correctly apply apple discounts if seven are purchased" in {
+    assert(SimpleCheckout.checkout(List.fill(7)("apple")) === BigDecimal(1))
+  }
+
+  "A checkout" should "correctly apply apple discounts if eight are purchased" in {
+    assert(SimpleCheckout.checkout(List.fill(8)("apple")) === BigDecimal(1))
+  }
+
+  "A checkout" should "correctly apply orange discounts if three are purchased" in {
+    assert(SimpleCheckout.checkout(List.fill(3)("orange")) === BigDecimal(4.10))
+  }
+
+  "A checkout" should "correctly apply orange discounts if eight are purchased" in {
+    assert(SimpleCheckout.checkout(List.fill(8)("orange")) === BigDecimal(12.3))
+  }
+
+  "A checkout" should "correctly apply orange discounts if nine are purchased" in {
+    assert(SimpleCheckout.checkout(List.fill(9)("orange")) === BigDecimal(12.3))
   }
 }
